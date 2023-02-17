2fa05820155f3f083fd2cb87b419f9a5acfc85c3
1997kb <princebudhathoki@gmail.com>
2023-02-17 12:29:44 +0545

    anagram solved

--- anagram.js
+++ anagram.js
@@ -3,24 +3,39 @@
 //or phrase, typically using all the original letters exactly once.
 
 
-const isAnagram = (s,t) => {
-    if( s.length != t.length){
-        return false
-    }
-
+function isAnagram(s, t) {
+  // If the two strings have different lengths, they cannot be anagrams
+  if (s.length !== t.length) {
+    return false;
+  }
+  
+  // Create a character frequency map for string s
+  const freqMap = {};
+  for (let i = 0; i < s.length; i++) {
     
-
+    const char = s[i];
    
-    let arr1 = [...new Set(s.split("").sort())]
-    let arr2 = [...new Set(t.split("").sort())]
-    if(JSON.stringify(arr1) === JSON.stringify(arr2)){
-        return true
-    }else{
-        return false
-    }
-
+    freqMap[char] = (freqMap[char] || 0) + 1;
     
-   
+  }
+  
+  
+  // Check if each character in string t is in the frequency map
+  for (let i = 0; i < t.length; i++) {
+    const char = t[i];
+    if (!freqMap[char]) {
+        console.log("returned from if statement")
+      return false;
+    } else {
+      console.log(freqMap[char])
+      freqMap[char]--;
+      console.log(freqMap)
+    }
+  }
+  
+  
+  // If we made it this far, all characters in t were found in s
+  return true;
 }
 
-console.log(isAnagram("aacc","ccac"));
+console.log(isAnagram("aacc","caca"));
